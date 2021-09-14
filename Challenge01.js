591b38b121ae458998205c68b399fb090a4b4ecd
Tay <tayk.dev@gmail.com>
2021-09-14 15:35:16 -0700

    Delete Challenge01.js

--- Challenge01.js
+++
@@ -1,9 +0,0 @@
-let massMark = 78;
-let massJohn = 92;
-let heightMark = 1.69;
-let heightJohn = 1.95;
-
-let markBMI =  massMark / (heightMark * heightMark);
-let johnBMI =  massJohn / (heightJohn * heightJohn);
-
-let markHigherBMI;
